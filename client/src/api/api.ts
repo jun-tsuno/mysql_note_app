4a9882319bb208c54033d9e76dca7958b28f423d
Jun <jun55tsuno@gmail.com>
2023-05-28 21:58:56 -0700

    vercel.json

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,3 +1,5 @@
 import axios from 'axios';
 
-export const api = axios.create({ baseURL: 'http://localhost:8000' });
+export const api = axios.create({
+	baseURL: 'https://mysql-note-app.vercel.app/',
+});
